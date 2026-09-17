cf786db1643b561a3985d64b8436b50a972ad7d9
yusuke.yoda <yusuke.yoda@givery.co.jp>
2026-09-17 18:33:14 +0900

    test: large plan, import block, tflint violation

--- environments/test1/main.tf
+++ environments/test1/main.tf
@@ -14,3 +14,28 @@ output "result" {
   description = "Result of the main module"
   value       = module.main
 }
+
+# --- CI verification (scratch, do not merge) ---
+
+# tflint gate: camelCase violates terraform_naming_convention (warning)
+variable "badName" {
+  type        = string
+  description = "Intentional naming violation for tflint gate check"
+  default     = "x"
+}
+
+# Large plan: push the PR comment past GitHub's size limit
+resource "random_id" "bulk" {
+  count       = 300
+  byte_length = 8
+}
+
+# Import block: exercise "N to import" parsing in the plan summary
+import {
+  to = random_id.imported
+  id = "p-9hUgAAAAA"
+}
+
+resource "random_id" "imported" {
+  byte_length = 8
+}
